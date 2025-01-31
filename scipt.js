3774d0eae517ad8381502a84879be6d3543ea1f0
narissau <narissau@byu.edu>
2025-01-31 16:51:50 -0700

    back to top button js

--- scipt.js
+++ scipt.js
@@ -18,3 +18,20 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const backToTop = document.getElementById("back-to-top");
+
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > 300) {
+            backToTop.style.display = "block";
+        } else {
+            backToTop.style.display = "none";
+        }
+    });
+
+    backToTop.addEventListener("click", function () {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    });
+});
